7e9f58c69598595c1b3e820638116f64bc2e426f
Miha Stele <stele1000@gmail.com>
2023-12-26 11:17:06 +0100

    add formatter

--- src/main.c
+++ src/main.c
@@ -1,4 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
-int main(int argc, char *argv[]) {
+bool initialize_window(void)
+{
+    /* Initialize SDL */
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640,
+                                          480,
+                                          0);
+
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    /* Create SDL window */
+
     return 0;
 }
